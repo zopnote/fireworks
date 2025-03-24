4ee45853f6b0e595bede72f1c5ad8e7fd9c4b7f3
Zopnote <lennysiebert08@gmail.com>
2025-03-24 15:20:02 +0100

    Set up project files

---
+++ engine/runtime/stdpch.h
@@ -0,0 +1,10 @@
+// Fireworks precompiled header
+
+
+#pragma once
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
---
+++ engine/runtime/tests/main_test.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <test.h>
+int main() {
+    printf("sum: %f", sum(5, 4));
+    return 0;
+}
