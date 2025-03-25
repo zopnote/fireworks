fef7e45fbacc05c414dc14913270203818a87fce
Zopnote <lennysiebert08@gmail.com>
2025-03-25 18:26:19 +0100

    Interim of build integration

--- engine/runtime/vendor/libyaml.cmake
+++ engine/runtime/vendor/libyaml.cmake
@@ -10,9 +10,12 @@ set(YAML_SOURCE_FILES
         ${CMAKE_BINARY_DIR}/_deps/libyaml/src/scanner.c
         ${CMAKE_BINARY_DIR}/_deps/libyaml/src/writer.c
 )
+if (NOT EXISTS ${CMAKE_BINARY_DIR}/_deps)
+    file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/_deps)
+endif ()
 
 if (NOT EXISTS ${YAML_DIRECTORY})
-    add_custom_target(yaml_clone
+    add_custom_target(vendor_yaml_clone
             COMMAND git clone https://github.com/zopnote/libyaml.git ${YAML_DIRECTORY} || true
             WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/_deps
             COMMENT "Cloning libyaml repository"
@@ -21,30 +24,30 @@ if (NOT EXISTS ${YAML_DIRECTORY})
     add_custom_command(
             OUTPUT ${YAML_SOURCE_FILES}
             COMMENT "Preparing YAML sources"
-            DEPENDS yaml_clone
+            DEPENDS vendor_yaml_clone
     )
 endif ()
 
-add_library(yaml SHARED ${YAML_SOURCE_FILES})
+add_library(vendor_yaml SHARED ${YAML_SOURCE_FILES})
 
 if (NOT EXISTS ${YAML_DIRECTORY})
-    add_dependencies(yaml yaml_clone)
+    add_dependencies(vendor_yaml vendor_yaml_clone)
 endif ()
 
-target_compile_definitions(yaml
+target_compile_definitions(vendor_yaml
         PRIVATE HAVE_CONFIG_H
         PUBLIC
         $<$<NOT:$<BOOL:${BUILD_SHARED_LIBS}>>:YAML_DECLARE_STATIC>
         $<$<BOOL:${MSVC}>:_CRT_SECURE_NO_WARNINGS>
 )
 
-set_target_properties(yaml
+set_target_properties(vendor_yaml
         PROPERTIES DEFINE_SYMBOL YAML_DECLARE_EXPORT
 )
 
-target_include_directories(yaml PUBLIC ${YAML_DIRECTORY}/include)
+target_include_directories(vendor_yaml PUBLIC ${YAML_DIRECTORY}/include)
 
-install(TARGETS yaml
+install(TARGETS vendor_yaml
         COMPONENT DESTINATION ${OUT_BINARY_DIRECTORY}
         RUNTIME DESTINATION ${OUT_BINARY_DIRECTORY}
         ARCHIVE DESTINATION ${OUT_LIBRARY_DIRECTORY}
