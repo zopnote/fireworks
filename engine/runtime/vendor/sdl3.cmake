77945337f6f3ca967cef8e37aefdc9d1ea2d6b8e
Zopnote <lennysiebert08@gmail.com>
2025-04-06 11:37:24 +0200

    added submodules

--- engine/runtime/vendor/sdl3.cmake
+++
@@ -1,8 +0,0 @@
-include(FetchContent)
-
-FetchContent_Declare(
-        sdl
-        GIT_REPOSITORY https://github.com/libsdl-org/SDL.git
-        GIT_TAG release-3.2.10
-)
-FetchContent_MakeAvailable(sdl)
