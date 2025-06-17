6c1ecb9b4cbc3da85e4881468bec2d22fb3533d8
zopnote <lennysiebert08@gmail.com>
2025-06-17 19:45:27 +0200

    Added deps

---
+++ app/bin/fireworks.bat
@@ -0,0 +1,3 @@
+@echo off
+cd /d "%~dp0"
+dart run fireworks.dart %*
--- app/bin/fireworks.dart
+++ app/bin/fireworks.dart
@@ -1,29 +1,25 @@
-/*
- * Copyright (c) 2025 Lenny Siebert
- *
- * This software is dual-licensed:
- *
- * 1. Open Source License:
- *    This program is free software: you can redistribute it and/or modify
- *    it under the terms of the GNU General Public License version 3
- *    as published by the Free Software Foundation.
- *    This program is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY. See the GNU General Public
- *    License for more details: https://www.gnu.org/licenses/gpl-3.0.en.html
- *
- * 2. Commercial License:
- *    A commercial license will be available at a later time for use in commercial products.
- */
 
+import 'package:fireworks/execution/doctor.dart';
+import 'package:fireworks/runner.dart';
+void main() {
 
-import 'package:fireworks/command/command.dart';
-import 'package:fireworks/command/root.dart';
-
-void main(List<String> args) async {
-  CommandError? err = await rootCommand.execute(args);
-  if (err != null) {
-    if (err.syntax) print(err.executedCmd.syntax());
-    if (err.errorMessage != null) print("\nError occurred: " + err.errorMessage!);
-  }
+  doctor(rootCommand, "", []);
 }
+void ads(List<String> args) => run(rootCommand,
+  globalFlags: [
+    Flag("verbose", description: "Prints out detailed information of the process."),
+    Flag("help", description: "Prints out help of a command.")
+  ]
+);
 
+Command rootCommand = Command(
+  "fireworks",
+  description: "Manage your fireworks projects with the corresponding tools.",
+  run: (cmd, _, _) => CommandResponse(syntax(cmd)),
+  subCommands: [
+    Command("doctor",
+        description: "Ensures the presence of crucial tools for fireworks in the environment.",
+        run: (_, _, _) => CommandResponse("asd")
+    ),
+  ]
+);
