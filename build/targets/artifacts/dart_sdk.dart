8cdd08c4ed3c185dbed56e6d1ec669e8c0b2a8ec
lenny <lennysiebert08@gmail.com>
2025-07-15 22:51:43 +0200

    test flow

--- build/targets/artifacts/dart_sdk.dart
+++ build/targets/artifacts/dart_sdk.dart
@@ -102,7 +102,6 @@ List<BuildStep> processSteps = [
       return !Directory(path.join(env.installDirectoryPath, "bin")).existsSync();
     },
     run: (env) {
-      return true;
     }
   ),
   BuildStep(
